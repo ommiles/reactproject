b8fdf9fee7d3582daf1a6e538a44fa79a4657a04
OM Miles <olivia.m.miles@gmail.com>
2021-01-30 16:04:33 -0800

    Update

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true, 
+    topLine: 'Topline Text Here',
+    headline: 'Headline in Larger Text Goes Here',
+    description: 'Paragraph tag text in longer form paragraph will go here.',
+    buttonLabel: 'Get a Quote',
+    imgStart: false,
+    img: '/images/mobile-description.png',
+    alt: 'Image',
+    dark: true,
+    primary: true,  
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'services',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false, 
+    topLine: 'Topline Text Here',
+    headline: 'Headline in Larger Text Goes Here',
+    description: 'Paragraph tag text in longer form paragraph will go here.',
+    buttonLabel: 'Get a Quote',
+    imgStart: true,
+    img: '/images/pablo-chars-1.png',
+    alt: 'Image',
+    dark: false,
+    primary: false,  
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'process',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true, 
+    topLine: 'Topline Text Here',
+    headline: 'Headline in Larger Text Goes Here',
+    description: 'Paragraph tag text in longer form paragraph will go here.',
+    buttonLabel: 'Get a Quote',
+    imgStart: false,
+    img: '/images/rights.png',
+    alt: 'Image',
+    dark: true,
+    primary: true,  
+    darkText: false
+}
